553cebdb9f4f0562a4aa7d7d822f701f9d4400d1
vikas-7 <57045486+vikas-7@users.noreply.github.com>
2019-12-08 15:21:10 +0530

    Update PRINT_MSG.c

--- PRINT_MSG.c
+++ PRINT_MSG.c
@@ -1,21 +1,17 @@
-
 #include "stm32f4xx.h"
-#include <string.h>
-#include <stdio.h>
-
-void printMsg(int a)
+void printMsg1(const int a)
 {
-	 
-	 char Msg1[100];
-	 
+	 char Msg[100];
 	 char *ptr;
-	 sprintf(Msg1, "%d, \n", a);
-	 
-	 ptr = Msg1 ;
-   while(*ptr != '\0'){
+	
+		 
+		sprintf(Msg, " Y = %x ", a);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
       ITM_SendChar(*ptr);
       ++ptr;
-   
-	 }
-	 ITM_SendChar('\t')	 ;
    }
+	
+	 
+}
